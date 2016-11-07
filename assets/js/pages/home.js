966acd18859b14b8ed01ec17e2584e968f186049
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-07 09:48:52 +0530

    image upload form version 2 final

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -3,33 +3,32 @@ $(document).ready(function() {
     console.log("App loaded !");
 });
 /**********image upload form hero toggle***********/
-$(document).ready(function() {
-    $('#img_upload_trigger').click(function() {
-        $('#img_upload_trigger').fadeOut(function() {
-            $('#img-upload-tabs').slideDown();
-            $('ul.tabs').tabs('select_tab', 'test1');
-        });
-    });
-});
-$(document).ready(function() {
-    $('#img-upload-close-1, #img-upload-close-2').click(function() {
-        $('#img-upload-tabs').slideUp(function() {
-            $('#img_upload_trigger').fadeIn();
-        });
-    });
-});
 /********************************************************/
 $(document).ready(function() {
     $('#img_upload').click(function() {
-      $('#dropzone_form').fadeIn(function(){
-        $('#url_upload').fadeOut();
-      });
+        $('#dropzone_form').fadeIn(function() {
+            $('#url_upload').fadeOut();
+        });
     });
 });
 $(document).ready(function() {
     $('#no_marg_up_down').click(function() {
-      $('#dropzone_form').slideUp(function(){
-        $('#url_upload').fadeIn();
-      });
+        $('#dropzone_form').slideUp(function() {
+            $('#url_upload').fadeIn();
+        });
+    });
+});
+$(document).ready(function() {
+    $('#url_upload').click(function() {
+        $('#url_form').fadeIn(function() {
+            $('#img_upload').fadeOut();
+        });
+    });
+});
+$(document).ready(function() {
+    $('#no_marg_up_down1').click(function() {
+        $('#url_form').slideUp(function() {
+            $('#img_upload').fadeIn();
+        });
     });
 });
