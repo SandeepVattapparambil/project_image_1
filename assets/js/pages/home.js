d0d3edf97d2621e41ff623fa68c7f184ffb2b760
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-07 11:35:24 +0530

    updates

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -7,7 +7,17 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('#img_upload').click(function() {
         $('#dropzone_form').fadeIn(function() {
-            $('#url_upload').fadeOut();
+            $('#url_upload').fadeOut(function() {
+                $('#pulse_div').pulsate({
+                    color: "#cddc39", // set the color of the pulse
+                    reach: 20, // how far the pulse goes in px
+                    speed: 1000, // how long one pulse takes in ms
+                    pause: 0, // how long the pause between pulses is in ms
+                    glow: true, // if the glow should be shown too
+                    repeat: false, // will repeat forever if true, if given a number will repeat for that many times
+                    onHover: false // if true only pulsate if user hovers over the element
+                });
+            });
         });
     });
 });
