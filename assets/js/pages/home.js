49229f880ece83c66414b31c14102eaae71a2911
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-12 22:25:26 +0530

    loader updates

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -119,3 +119,36 @@ $(document).ready(function() {
     });
 });
 /***************************************************/
+/*Detect Mobile*/
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+/*if mobile show loader*/
+if(isMobile.any()) {
+   //alert("This is a Mobile Device");
+   $(".fakeLoader").fakeLoader({
+       timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
+       zIndex: "9999", //Default zIndex
+       //spinner: "spinner1", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+       bgColor: "#cddc39 ", //Hex, RGB or RGBA colors
+       imagePath: "assets/images/logo.png" //If you want can you insert your custom image
+   });
+}
+/********************************/
