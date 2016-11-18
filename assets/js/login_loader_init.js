10f68cfab03f3affeff9981b73c0d18f60da6d72
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-18 21:06:21 +0530

    updates

--- assets/js/login_loader_init.js
+++ assets/js/login_loader_init.js
@@ -1,42 +1,39 @@
 /*****************Login******************************/
 $(document).ready(function() {
-    loadpage();
     console.log("App loaded !");
 });
+/***************************************************/
 
-function loadpage() {
-    /*Detect Mobile & Handheld*/
-    var isMobile = {
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function() {
-            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-        }
-    };
-    /*if mobile then show loader*/
-    if (isMobile.any()) {
-        //alert("This is a Mobile Device");
-        $(".device-loader").fakeLoader({
-            timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
-            zIndex: "9999", //Default zIndex
-            //spinner: "spinner1", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
-            bgColor: "#cddc39 ", //Hex, RGB or RGBA colors
-            imagePath: "assets/images/logo.png" //If you want can you insert your custom image
-        });
-        console.log('Mobile!');
+function getDeviceType() {
+    if (navigator.userAgent.match(/Android/i)) {
+        var device = 'Android';
+        return device;
+    } else if (navigator.userAgent.match(/BlackBerry/i)) {
+        var device = 'Blackberry';
+        return device;
+    } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+        var device = 'iOS';
+        return device;
+    } else if (navigator.userAgent.match(/Opera Mini/i)) {
+        var device = 'Opera';
+        return device;
+    } else if (navigator.userAgent.match(/IEMobile/i)) {
+        var device = 'Windows';
+        return device;
+    } else {
+        var device = 'Undefined';
+        return device;
     }
 }
-/***************************************************/
+
+function loadPage() {
+    console.log("This is a Mobile Device");
+    $(".device-loader").fakeLoader({
+        timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
+        zIndex: "9999", //Default zIndex
+        //spinner: "spinner1", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+        bgColor: "#cddc39 ", //Hex, RGB or RGBA colors
+        imagePath: "assets/images/logo.png" //If you want can you insert your custom image
+    });
+    console.log('Mobile!');
+}
--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,6 +1,27 @@
 /*****set width of button on login card*/
 $(document).ready(function() {
     var width = $('#facebook').width() + 50;
+    /*Detect Mobile & Handheld*/
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
     if (isMobile.any()) {
         $('#google, #twitter, #facebook, #email').width(width - 30);
     } else {
