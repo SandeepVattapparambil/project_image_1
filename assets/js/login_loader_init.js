2b79657fa9ed20ae8929118ac11700d7de39b8f1
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-18 21:13:29 +0530

    updates

--- assets/js/login_loader_init.js
+++ assets/js/login_loader_init.js
@@ -1,8 +1,26 @@
 /*****************Login******************************/
 $(document).ready(function() {
+    loadPage();
     console.log("App loaded !");
 });
 /***************************************************/
+function loadPage() {
+    console.log("Identify device");
+    var device = getDeviceType();
+    if (device != '') {
+        console.log("Device identified as a Mobile");
+        $(".device-loader").fakeLoader({
+            timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
+            zIndex: "9999", //Default zIndex
+            //spinner: "spinner1", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
+            bgColor: "#cddc39 ", //Hex, RGB or RGBA colors
+            imagePath: "assets/images/logo.png" //If you want can you insert your custom image
+        });
+    }
+    else{
+      console.log("Device identified as PC");
+    }
+}
 
 function getDeviceType() {
     if (navigator.userAgent.match(/Android/i)) {
@@ -21,19 +39,7 @@ function getDeviceType() {
         var device = 'Windows';
         return device;
     } else {
-        var device = 'Undefined';
+        var device = '';
         return device;
     }
-}
-
-function loadPage() {
-    console.log("This is a Mobile Device");
-    $(".device-loader").fakeLoader({
-        timeToHide: 1200, //Time in milliseconds for fakeLoader disappear
-        zIndex: "9999", //Default zIndex
-        //spinner: "spinner1", //Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7'
-        bgColor: "#cddc39 ", //Hex, RGB or RGBA colors
-        imagePath: "assets/images/logo.png" //If you want can you insert your custom image
-    });
-    console.log('Mobile!');
 }
