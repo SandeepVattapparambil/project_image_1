7cd62994d9507608299c856e48e190a3d8b6a7cd
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-18 20:46:20 +0530

    updates

--- assets/js/login_loader_init.js
+++ assets/js/login_loader_init.js
@@ -5,6 +5,27 @@ $(document).ready(function() {
 });
 
 function loadpage() {
+    /*Detect Mobile & Handheld*/
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
     /*if mobile then show loader*/
     if (isMobile.any()) {
         //alert("This is a Mobile Device");
@@ -19,26 +40,3 @@ function loadpage() {
     }
 }
 /***************************************************/
-/*Detect Mobile & Handheld*/
-var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-
-/********************************/
