3f1d034e599498d0dd2669272eadf82dd2a3af01
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-11-29 12:18:32 +0530

    browser-sync updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+var reload = bs.reload;
 
-// Static server
 gulp.task('browser-sync', function() {
-    browserSync.init({
+    bs.init({
         server: {
             baseDir: "./"
         }
     });
 });
+
+// Watch files for changes
+gulp.task('watch', ['browser-sync'], function() {
+    // Watch HTML files
+    gulp.watch('*.html', reload);
+    // Watch css files
+    gulp.watch('assets/css/*', ['css']);
+    // Watch JS files
+    gulp.watch('assets/js/*', ['js']);
+    // Watch image files
+    gulp.watch('assets/images/*', ['images']);
+});
+// Default task
+gulp.task('default', ['watch', 'browser-sync']);
