9a36e7be045135433ef9b139bf097f5f931a45cd
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-01 21:27:29 +0530

    js file updates

--- assets/js/pages/profile.js
+++ assets/js/pages/profile.js
@@ -1,1 +1,26 @@
 /******************Profile settings***********************/
+/******************tooltipster********************/
+$(document).ready(function() {
+  //
+  $('#bot').addClass('animated bounceIn');
+    $('#bot').hover(function(){
+      $(this).addClass('bot-bounce-vertical');
+    });
+    //
+    $('.tooltip').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-punk',
+        side: 'right'
+    });
+});
+/********Bot*********/
+$(document).ready(function() {
+    $('.bot-btn').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-borderless',
+        side: 'left'
+    });
+});
+/**********************************************/
