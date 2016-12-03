afb3c3dab9bf56eaa98c0cddd6611e019fdf3964
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-03 22:04:27 +0530

    updates

--- assets/js/pages/profile.js
+++ assets/js/pages/profile.js
@@ -1,9 +1,9 @@
 /******************Profile settings***********************/
 $(document).ready(function() {
-  //Bot button animation on hover
-  $('#bot').addClass('animated bounceIn');
-    $('#bot').hover(function(){
-      $(this).addClass('bot-bounce-vertical');
+    //Bot button animation on hover
+    $('#bot').addClass('animated bounceIn');
+    $('#bot').hover(function() {
+        $(this).addClass('bot-bounce-vertical');
     });
     //Tooltip punk style
     $('.tooltip').tooltipster({
@@ -20,4 +20,33 @@ $(document).ready(function() {
         side: 'left'
     });
 
+
+    //Sparkline graphs
+    var DrawSparkline = function() {
+        var linePoints = [0, 1, 3, 2, 1, 1, 4, 1, 2, 0, 3, 1, 3, 4, 1, 0, 2, 3, 6, 3, 4, 2, 7, 5, 2, 4, 1, 2, 6, 13, 4, 2];
+        $('#sparkline-line1, #sparkline-line2, #sparkline-line3, #sparkline-line4').sparkline(linePoints, {
+            type: 'line',
+            width: 'calc(100% + 4px)',
+            height: '45',
+            chartRangeMax: 13,
+            lineWidth: 2,
+            spotRadius: 3,
+            spotColor: '#827717',
+            minSpotColor: '#03a9f4',
+            maxSpotColor: '#03a9f4',
+            lineColor: '#afb42b',
+            fillColor: '#f0f4c3',
+            highlightLineColor: '#c0ca33',
+            highlightSpotColor: '#827717',
+            tooltip: true
+        });
+    };
+    DrawSparkline();
+    var resizeChart;
+    $(window).resize(function(e) {
+        clearTimeout(resizeChart);
+        resizeChart = setTimeout(function() {
+            DrawSparkline();
+        }, 300);
+    });
 });
