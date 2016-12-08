0adae9ffb875427d2be6dcc66266dd4e7869985e
Sandeep Vattapparambil <sandeepv68@gmail.com>
2016-12-08 22:00:14 +0530

    updates

---
+++ assets/js/pages/messages.js
@@ -0,0 +1,26 @@
+/*************Messages****************/
+$(document).ready(function() {
+    //Bot button bounce
+    $('#bot').addClass('animated bounceIn');
+    $('#bot').hover(function() {
+        $(this).addClass('bot-bounce-vertical');
+    });
+    //Image hover icon
+    $('.image-activator').hover(function() {
+        $(this).parent().find('p').toggle('fast');
+    });
+    //tooltip init
+    $('.tooltip').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-borderless',
+        side: 'bottom'
+    });
+    //tooltip init for bot
+    $('.bot-btn').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-borderless',
+        side: 'left'
+    });
+});
